bff747cd60adfeb30dd679b4943234ef0e651660
blueyghes <gegeworks16@gmail.com>
2026-05-28 00:27:53 +0700

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,1 +1,159 @@
+// LOGIN
 
+const loginForm =
+document.getElementById("loginForm");
+
+if(loginForm){
+
+  loginForm.addEventListener(
+  "submit",
+
+  function(e){
+
+    e.preventDefault();
+
+    const username =
+    document.getElementById("username").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    const savedUser =
+    JSON.parse(
+      localStorage.getItem("registeredUser")
+    );
+
+    if(
+
+      (username === "admin" &&
+      password === "123")
+
+      ||
+
+      (
+
+        savedUser &&
+        username === savedUser.username &&
+        password === savedUser.password
+
+      )
+
+    ){
+
+      localStorage.setItem(
+        "loggedInUser",
+        username
+      );
+
+      alert("Login berhasil!");
+
+      window.location.href =
+      "../index.html";
+
+    }else{
+
+      alert(
+        "Username atau password salah!"
+      );
+
+    }
+
+  });
+
+}
+
+// REGISTER
+
+const registerForm =
+document.getElementById("registerForm");
+
+if(registerForm){
+
+  registerForm.addEventListener(
+  "submit",
+
+  function(e){
+
+    e.preventDefault();
+
+    const username =
+    document.getElementById("username").value;
+
+    const email =
+    document.getElementById("email").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    localStorage.setItem(
+
+      "registeredUser",
+
+      JSON.stringify({
+
+        username,
+        email,
+        password
+
+      })
+
+    );
+
+    alert("Register berhasil!");
+
+    window.location.href =
+    "login.html";
+
+  });
+
+}
+
+// FORGOT PASSWORD
+
+const forgotForm =
+document.getElementById("forgotForm");
+
+if(forgotForm){
+
+  forgotForm.addEventListener(
+  "submit",
+
+  function(e){
+
+    e.preventDefault();
+
+    const username =
+    document.getElementById(
+      "forgotUsername"
+    ).value;
+
+    const savedUser =
+    JSON.parse(
+      localStorage.getItem(
+        "registeredUser"
+      )
+    );
+
+    if(
+
+      savedUser &&
+      username === savedUser.username
+
+    ){
+
+      alert(
+        "Password kamu adalah: " +
+        savedUser.password
+      );
+
+    }else{
+
+      alert(
+        "Username tidak ditemukan!"
+      );
+
+    }
+
+  });
+
+}
