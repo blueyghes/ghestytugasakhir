71924761ab86b10a9af873bd9c42bad3d2493f6b
blueyghes <gegeworks16@gmail.com>
2026-05-28 00:23:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
+const slider =
+document.querySelector(".slider");
 
+// NEXT
+document.querySelector(".next")
+.addEventListener("click", () => {
+
+  slider.scrollBy({
+    left:400,
+    behavior:"smooth"
+  });
+
+});
+
+// PREV
+document.querySelector(".prev")
+.addEventListener("click", () => {
+
+  slider.scrollBy({
+    left:-400,
+    behavior:"smooth"
+  });
+
+});
+
+// SCROLL
+function scrollPortfolio(){
+
+  document.querySelector("#portfolio")
+  .scrollIntoView({
+    behavior:"smooth"
+  });
+
+}
+
+// LOGIN LOGOUT
+
+const authArea =
+document.getElementById("authArea");
+
+const currentUser =
+localStorage.getItem("loggedInUser");
+
+if(currentUser){
+
+  authArea.innerHTML = `
+
+    <div class="user-box">
+
+      <span>
+        Hi, ${currentUser}
+      </span>
+
+      <button
+      onclick="logout()"
+      class="logout-btn">
+
+        Logout
+
+      </button>
+
+    </div>
+
+  `;
+
+}
+
+// LOGOUT
+function logout(){
+
+  localStorage.removeItem(
+    "loggedInUser"
+  );
+
+  alert("Logout berhasil!");
+
+  window.location.reload();
+
+}
